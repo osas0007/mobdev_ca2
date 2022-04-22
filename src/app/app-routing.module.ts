4bb587b0b2fcb18858362d3d75fb2ffa206cee20
osas0007 <2021263@student.cct.ie>
2022-04-22 11:35:48 +0000

    planet details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
   {
     path: 'people-details',
     loadChildren: () => import('./pages/people-details/people-details.module').then( m => m.PeopleDetailsPageModule)
+  },
+  {
+    path: 'planet-details',
+    loadChildren: () => import('./pages/planet-details/planet-details.module').then( m => m.PlanetDetailsPageModule)
   }
 ];
 
